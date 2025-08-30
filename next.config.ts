70e6e6461c69e8142a297c17cc9ea11cca7dcc2f
Parmar Kalpesh <parmarkalpesh1586@gmail.com>
2025-08-30 17:30:17 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- next.config.ts
+++ next.config.ts
@@ -27,6 +27,12 @@ const nextConfig: NextConfig = {
         hostname: 'encrypted-tbn0.gstatic.com',
         port: '',
         pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'idx.google.com',
+        port: '',
+        pathname: '/**',
       }
     ],
   },
