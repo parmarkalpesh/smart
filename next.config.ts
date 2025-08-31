7458371fa36ee21fe5fc975689668614813abbc8
Parmar Kalpesh <parmarkalpesh1586@gmail.com>
2025-08-31 00:40:30 +0000

    Error: Invalid src prop (https://cdn.thewirecutter.com/wp-content/media/

--- next.config.ts
+++ next.config.ts
@@ -45,6 +45,12 @@ const nextConfig: NextConfig = {
         hostname: 'lh3.googleusercontent.com',
         port: '',
         pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'cdn.thewirecutter.com',
+        port: '',
+        pathname: '/**',
       }
     ],
   },
